e7460c148f36e9451ef299703615d25cdce88e0d
jvican <jorgevc@fastmail.es>
2016-02-17 23:48:01 +0100

    Add projection via `HListParser` and test suite
    * Add the second step of the algorithm to make projection
    
    * Add full test suite covering the parsers implemented by HList
    
    * Polish old tests and renamings some variables
    
    * Introduce the magic of type-level programming!

--- src/main/scala/parsec/HListParsers.scala
+++ src/main/scala/parsec/HListParsers.scala
@@ -1,63 +1,83 @@
 package parsec
 
 trait HListParsers extends Parsers with RepetitionParsers {
+
   import shapeless._
   import ops.hlist._
+  import syntax.singleton._
 
+  /* Define Boolean on the type level */
+  val (wTrue, wFalse) = (true.witness, false.witness)
+  type True = wTrue.T
+  type False = wFalse.T
 
-  /**
-   * Proof that parsing an HList of parsers
-   * yields a result of parsers
-   */
-  trait Parsable[L <: HList, Res <: HList] {
+  /** Proof that parsing an HList of parsers yields a result */
+  trait Parsable[L <: HList, B <: HList, Res <: HList] {
     def parse(ps: L, in: Input): ParseResult[Res]
   }
 
-  /**
-   * Parsing a single parser gives its result
-   */
-  implicit def singleParsable[T] = {
-    new Parsable[Parser[T] :: HNil, T :: HNil] {
-      def parse(ps: Parser[T] :: HNil, in: Input): ParseResult[T :: HNil]
-        = ps.head(in).map { x => x :: HNil }
+  /** Parsing a single parser with True gives a result (base case proof) */
+  implicit def baseCaseParsableTrue[T] = {
+    new Parsable[Parser[T] :: HNil, True :: HNil, T :: HNil] {
+      def parse(ps: Parser[T] :: HNil, in: Input): ParseResult[T :: HNil] = 
+        ps.head(in).map { x => x :: HNil }
     }
   }
 
-  /**
-   * Parsing recursively works too
-   */
-  implicit def recParsable[T, TS <: HList, Res <: HList]
-                          (implicit parsable: Parsable[TS, Res]) = {
-    new Parsable[Parser[T] :: TS, T :: Res] {
-      def parse(ps: Parser[T] :: TS, in: Input): ParseResult[T :: Res] = {
-        val (hd, tl) = (ps.head, ps.tail)
-        hd(in) match {
-          case f @ Failure(_, _) => f
-          case Success(a, rest) => parsable.parse(tl, rest) match {
-            case f @ Failure(msg, _) => Failure(msg, in)
-            case Success(as, rest2) => Success(a :: as, rest2)
+  /** Parsing a single parser with False gives a result (base case proof) */
+  implicit def baseCaseParsableFalse[T] = {
+    new Parsable[Parser[T] :: HNil, False :: HNil, HNil] {
+      def parse(ps: Parser[T] :: HNil, in: Input): ParseResult[HNil] = 
+        ps.head(in).map { _ => HNil }
+    }
+  }
+
+  /** Parsing multiple parsers with a True attached will ignore 
+   *  their result but still provide a result of the Parser. */
+  implicit def recursiveParsableTrue
+    [T, TS <: HList, BS <: HList, Res <: HList]
+    (implicit parsable: Parsable[TS, BS, Res]) = {
+      new Parsable[Parser[T] :: TS, True :: BS, T :: Res] {
+        def parse(ps: Parser[T] :: TS, in: Input): ParseResult[T :: Res] = {
+          ps.head(in) match {
+            case f @ Failure(_, _) => f
+            case Success(a, rest) => parsable.parse(ps.tail, rest) match {
+              case f @ Failure(msg, _) => Failure(msg, in)
+              case Success(as, rest2) => Success(a :: as, rest2)
+            }
           }
         }
       }
     }
-  }
 
-  /**
-   * parsing a HList of parsers,
-   * gives us a HList of results
-   */
-  def parse[L <: HList, Res <: HList]
-           (xs: L, in: Input)
-           (implicit parsable: Parsable[L, Res]): ParseResult[Res] =
-    parsable.parse(xs, in)
+  /** Parsing multiple parsers with a False attached will ignore 
+   *  their result but still provide a result of the Parser. */
+  implicit def recursiveParsableFalse
+    [T, TS <: HList, BS <: HList, Res <: HList]
+    (implicit parsable: Parsable[TS, BS, Res]) = {
+      new Parsable[Parser[T] :: TS, False :: BS, Res] {
+        def parse(ps: Parser[T] :: TS, in: Input): ParseResult[Res] = {
+          ps.head(in) match {
+            case f @ Failure(_, _) => f
+            case Success(_, rest) => parsable.parse(ps.tail, rest) match {
+              case f @ Failure(msg, _) => Failure(msg, in)
+              case Success(as, rest2) => Success(as, rest2)
+            }
+          }
+        }
+      }
+    }
 
-  /**
-   * lift an HList of parsers into a parser
-   * ATTENTION, the parameter needs to be lazy for some reason,
-   * haven't figured it out yet
-   */
-  def mkParser[L <: HList, Res <: HList](xs: => L)
-              (implicit parsable: Parsable[L, Res]): Parser[Res] = Parser { in =>
-    parsable.parse(xs, in)
-  }
+  /** Parsing a HList of parsers gives us a HList of results */
+  def parse[L <: HList, B <: HList, Res <: HList](xs: L, in: Input)
+    (implicit parsable: Parsable[L, B, Res]): ParseResult[Res] =
+      parsable.parse(xs, in)
+
+  /** Lift an HList of parsers into a parser
+   *
+   * TODO Figure out why the parameter needs to be lazy */
+  def mkParser[L <: HList, B <: HList, Res <: HList](xs: => L)
+    (implicit parsable: Parsable[L, B, Res]): Parser[Res] = 
+      Parser { in => parsable.parse(xs, in) }
 }
+
