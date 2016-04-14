d690c20607444b9bd1c8c422db9a9628d64fd8b8
manojo <manojo10386@gmail.com>
2016-04-14 18:50:12 +0200

    separate into multiple projects

--- project/Build.scala
+++ project/Build.scala
@@ -1,66 +1,67 @@
 import sbt._
 import Keys._
-import com.typesafe.sbt.SbtScalariform
 
-trait BuildAndSettings extends Build {
-  val buildSettings = super.settings ++ Seq(
-    organization := "org.scalamacros",
-    version := "1.0.0",
-    scalaVersion := "2.11.7",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.10.5", "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6", "2.11.7"),
-    resolvers += Resolver.sonatypeRepo("snapshots"),
-    resolvers += Resolver.sonatypeRepo("releases"),
-    scalacOptions ++= Seq(
-      //"-optimize",
-      "-deprecation",
-      "-feature",
-      "-language:higherKinds",
-      "-language:implicitConversions"
+object ParsequeryBuild extends Build {
+  scalaVersion := "2.11.8"
+
+  def commonSettings = Seq(
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.11.8",
+    //scalacOptions := Seq("-optimize"), // <-- slows down compilation drastically
+    libraryDependencies ++=  Seq(
+      "org.scala-lang" % "scala-compiler"  % scalaVersion.value % "provided",
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value
+    ),
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
     )
   )
-}
 
-object ParsequeryBuild extends BuildAndSettings {
+  lazy val Benchmark = config("bench") extend Test
+
   lazy val root: Project = Project(
     "root",
     file("."),
-    settings = buildSettings ++ Seq(
+    settings = commonSettings ++ Seq(
       run <<= run in Compile in core)
   ) aggregate(macros, core)
 
   lazy val macros: Project = Project(
-    "macros",
-    file("macros"),
-    settings = buildSettings ++ Seq(
-      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
-      libraryDependencies := {
-        CrossVersion.partialVersion(scalaVersion.value) match {
-          // if Scala 2.11+ is used, quasiquotes are available in the standard distribution
-          case Some((2, scalaMajor)) if scalaMajor >= 11 =>
-            libraryDependencies.value
-          // in Scala 2.10, quasiquotes are provided by macro paradise
-          case Some((2, 10)) =>
-            libraryDependencies.value ++ Seq(
-              compilerPlugin("org.scalamacros" % "paradise" % "2.1.0-M5" cross CrossVersion.full),
-              "org.scalamacros" %% "quasiquotes" % "2.1.0-M5" cross CrossVersion.binary)
-        }
-      }
+    id = "macros",
+    base = file("macros"),
+    settings = commonSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "org.scalatest" %% "scalatest" % "3.0.0-M15" % "test",
+        "org.scalacheck" %% "scalacheck" % "1.11.5" % "test",
+        "com.storm-enroute" %% "scalameter" % "0.7",
+        "com.chuusai" %% "shapeless" % "2.2.5",
+        "com.lihaoyi" %% "pprint" % "0.3.8",
+        "com.lihaoyi" %% "fastparse" % "0.3.7"
+      ),
+
+      testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
+      parallelExecution in Benchmark := false,
+      parallelExecution in Test := false,
+      logBuffered := false
     )
+  ) configs(
+    Benchmark
+  ) settings(
+    inConfig(Benchmark)(Defaults.testSettings): _*
   )
 
   lazy val core: Project = Project(
-    "core",
-    file("core"),
-    settings = buildSettings ++ SbtScalariform.scalariformSettings ++ Seq(
-      testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework"),
-      libraryDependencies := {
-        libraryDependencies.value ++ Seq(
-          "com.lihaoyi" %% "fastparse" % "0.3.4",
-          "com.storm-enroute" %% "scalameter" % "0.7"
-        )
-      }
+    id = "Examples",
+    base = file("Examples"),
+    dependencies = Seq(macros),
+    settings = commonSettings ++ Seq(
+      // include the macro classes and resources in the main jar
+      mappings in (Compile, packageBin) ++= mappings.in(macros, Compile, packageBin).value,
+      // include the macro sources in the main source jar
+      mappings in (Compile, packageSrc) ++= mappings.in(macros, Compile, packageSrc).value
     )
-  ) dependsOn(macros)
+  )
 }
 
 /*name := "functadelic"
