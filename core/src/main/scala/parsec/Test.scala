461ee90b5093e1bfef7a9cdcb442404bfb12d96d
manojo <manojo10386@gmail.com>
2016-06-01 16:43:52 +0200

    parsing json, in the macro!

--- core/src/main/scala/parsec/Test.scala
+++ core/src/main/scala/parsec/Test.scala
@@ -33,30 +33,54 @@ object TestFastParse {
 
 object Test extends OptimisedParsers {
 
-//  val bool = accept("true".toArray) | accept("false".toArray)
-//  val skipped = skipWs(comma)
+  object JsonParser {
 
-//  import scala.collection.mutable.ArrayBuffer
-//  val manyBools = parseMany[ArrayBuffer[String]](List(
-//    (accept('['), true),
-//    (repsepFold(bool, skipped).toArrayBufferF, false),
-//    (accept(']'), true)
-//  ))
-  val myParser = optimise {
-    accept("greetings")// ~ accept("lion")
+    sealed abstract class JSValue
+    case class JSObject(dict: List[(String, JSValue)]) extends JSValue
+    case class JSArray(arr: List[JSValue]) extends JSValue
+    case class JSDouble(d: Int) extends JSValue
+    case class JSString(s: String) extends JSValue
+    case class JSBool(b: Boolean) extends JSValue
+    case object JSNull extends JSValue
+
+    val jsonParser = optimise {
+
+      def main: Parser[JSValue] = (
+        //obj |
+        arr |
+        //stringLiteral.map(x => JSString(x)) |
+        number.map(x => JSDouble(x)) |
+        accept("null").map(_ => JSNull) |
+        accept("true").map(_ => JSBool(true)) |
+        accept("false").map(_ => JSBool(false))
+      )
+
+//      def obj: Parser[JSValue] = (
+//        skipWs(accept('{')) ~> repsep(member, skipWs(accept(','))) <~ skipWs(accept('}'))
+//      ) map { x => JSObject(x) }
+//
+      def arr: Parser[JSValue] = (
+        skipWs(accept('[')) ~> repsep(main, skipWs(accept(','))) <~ skipWs(accept(']'))
+      ) map { x => JSArray(x) }
+//
+//      def member: Parser[(String, JSValue)] =
+//        stringLiteral ~ (skipWs(accept(':')) ~> main)
+
+      main
+    }
   }
 
 
   def main(args: Array[String]): Unit = {
-    println("greetings liaaon")
+    println("greetings lion")
 
     import scala.io.Source
     val fileName = "data/booleans-6600.json"
     val fileContent = Source.fromFile(fileName).mkString
     //val myReader = CharReader(fileContent.toArray)
-    val myReader = CharReader("greetings".toArray)
+    val myReader = CharReader("[ true, 123, [false, []] ]".toArray)
 
-    val Success(res, rest) = myParser(myReader)
+    val Success(res, rest) = JsonParser.jsonParser(myReader)
     println(res)
   }
 
--- macros/src/test/scala/parsec/TestCharParsers.scala
+++ macros/src/test/scala/parsec/TestCharParsers.scala
@@ -31,7 +31,8 @@ class CharParsersSuite
     val zero = CharReader("".toArray)
     val biggerNum = CharReader("12345".toArray)
 
-    checkSuccess(number, zero)(expected = 0, expectedPos = 0)
+    /** an empty string should not be a number */
+    checkFailure(number, zero)
     checkSuccess(number, biggerNum)(expected = 12345, expectedPos = 5)
   }
 
