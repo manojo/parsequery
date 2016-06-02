6f8688cc81798e544b7c390694900042d4163850
manojo <manojo10386@gmail.com>
2016-06-02 11:10:59 +0200

    add json parser that parses all

--- macros/src/test/scala/parsec/optimised/TestOptimisedParsers.scala
+++ macros/src/test/scala/parsec/optimised/TestOptimisedParsers.scala
@@ -209,4 +209,76 @@ class OptimisedParserSuite
     )
   }
 
+  test("json parser works") {
+
+    sealed abstract class JSValue
+    case class JSObject(dict: List[(String, JSValue)]) extends JSValue
+    case class JSArray(arr: List[JSValue]) extends JSValue
+    case class JSDouble(d: Int) extends JSValue
+    case class JSString(s: String) extends JSValue
+    case class JSBool(b: Boolean) extends JSValue
+    case object JSNull extends JSValue
+
+    val jsonParser = optimise {
+
+      def main: Parser[JSValue] = (
+        obj |
+        arr |
+        stringLiteral.map(x => JSString(x)) |
+        number.map(x => JSDouble(x)) |
+        accept("null").map(_ => JSNull) |
+        accept("true").map(_ => JSBool(true)) |
+        accept("false").map(_ => JSBool(false))
+      )
+
+      def obj: Parser[JSValue] = (
+        skipWs(accept('{')) ~> repsep(member, skipWs(accept(','))) <~ skipWs(accept('}'))
+      ) map { x => JSObject(x) }
+
+      def arr: Parser[JSValue] = (
+        skipWs(accept('[')) ~> repsep(main, skipWs(accept(','))) <~ skipWs(accept(']'))
+      ) map { x => JSArray(x) }
+
+      def member: Parser[(String, JSValue)] =
+        stringLiteral ~ (skipWs(accept(':')) ~> main)
+
+      main
+    }
+
+    val bool = "true".toArray
+    val strLit = "\"Roger pon top\"".toArray
+    val num = "123".toArray
+    val oneObj = """ {
+      "name" : {
+        "first": "Roger",
+        "last": "Federer"
+      }
+    }""".toArray
+
+    checkSuccess(jsonParser, CharReader(bool))(
+      expected = JSBool(true),
+      expectedPos = bool.length
+    )
+
+    checkSuccess(jsonParser, CharReader(strLit))(
+      expected = JSString("Roger pon top"),
+      expectedPos = strLit.length
+    )
+
+    checkSuccess(jsonParser, CharReader(num))(
+      expected = JSDouble(123),
+      expectedPos = num.length
+    )
+
+    checkSuccess(jsonParser, CharReader(oneObj))(
+      expected = JSObject(List(("name", JSObject(
+        List(
+          ("first", JSString("Roger")),
+          ("last", JSString("Federer"))
+        )
+      )))),
+      expectedPos = oneObj.length
+    )
+  }
+
 }
